595c1a49869551e45be3966d3296a36db2d95523
SmithJez <pingmihawk@gmail.com>
2017-09-24 14:14:36 +0700

    mit2

--- javascripts/ping.js
+++ javascripts/ping.js
@@ -1,4 +1,4 @@
-goog.require('proto.MsgLocalizedString');
+goog.require('proto.MsgGameData');
 goog.require('goog.dom');
 
 function sayHi() {
@@ -16,7 +16,7 @@ function GetString(file) {
 		
 	}
 	
-	var newHeader = new proto.MsgString.deserializeBinary (999);
+	var newHeader = new proto.msggamedata.MsgMonsterType.deserializeBinary (999);
   
 	goog.dom.appendChild(document.body, newHeader);
 }
@@ -32,16 +32,87 @@ function readUrl(url) {
 	for (i = 0; i < this.stream.length; i++) {
 		bytes[i] = this.stream.charCodeAt(i) & 0xff;
 	}
+
+	var stringFile = "/data/string.en.pb"
+	var reqString = new XMLHttpRequest();
+	reqString.open('GET', stringFile, false);
+	reqString.overrideMimeType('text/plain; charset=x-user-defined');
+	reqString.send(null);
+	if (reqString.status != 200) throw '[' + reqString.status + ']' + reqString.statusText;
+	this.streamString = reqString.responseText;
+	var bytesString = [];
+	for (i = 0; i < this.streamString.length; i++) {
+		bytesString[i] = this.streamString.charCodeAt(i) & 0xff;
+	}
 	
-	console.log(bytes);
+	var monster = new proto.MsgGameData.deserializeBinary (bytes);
+	var stringText = new proto.MsgString.deserializeBinary (bytesString);
+	
+	/* console.log(stringText.array[0][100]); */
+	
+	var arrayString = stringText.array[0];
+	console.log(arrayString.length);
+	var lookup = {};
+	for (var i = 0; i < arrayString.length; i++) {
+		/* console.log(arrayString[i][i]); */
+		lookup[arrayString[i][0]] = arrayString[i][1];
+		
+		/* console.log(arrayString[i][0]); */
+		
+		/* console.log(lookup[arrayString[i]]); */
+	}
+	
+/* 	var arr=arrayString.map(function(value){
+		return {
+			Key: value
+		}
+		
+	}); */
 	
 	
-	var text = new proto.MsgString.deserializeBinary (bytes);
-  var newHeader = goog.dom.createDom('h1', {'style': 'background-color:#EEE'},
-    text.toString());
-	/* console.log(newHeader.array.toString()); */
+	var arr = [];
+	for (var i = 0; i < arrayString.length; i++) 
+	{
+		arr.push( {
+			Key: arrayString[i][0][0],
+			value: arrayString[i][0][1]
+		})
+			
+	}
 	
-	goog.dom.appendChild(document.body, newHeader);
+	
+	/* console.log (lookup); */
+	/* console.log (arr); */
+	console.log (lookup[1871302204]);
+	
+	
+/* 	var result = $.grep(monster.array, function(e){ return e[0] == 0; });
+	console.log(result); */
+	
+	console.log(monster.array);
+	
+	for(i = 0; i < monster.array[3].length; i++) {
+		var newHeader = goog.dom.createDom('h4', {'style': 'background-color:#EEE'},
+			lookup[monster.array[3][i][1]]);
+			
+			console.log (lookup[monster.array[3][i][1]]);
+			/* console.log (lookup[monster.array[3][i][1]]); */
+			/* console.log(monster.array[3][i][1]); */
+			
+		goog.dom.appendChild(document.body, newHeader);	
+	}
+	
 	
 	return bytes;
+}
+
+
+function buildMap (obj) {
+	let map = new Map();
+	Object.keys(obj).forEach(key => {
+		map.set(key, obj[key]);
+	});
+	return map;
+	
+	
 }
